e1ee3cded2961cd86e5fcbfa16250a3705df598b
joelparrat <joel.parrat@gmail.com>
2018-12-26 18:07:30 +0100

    plus test bandeau

--- js/script.js
+++ js/script.js
@@ -2,6 +2,28 @@ $
 (
 	function()
 	{
+		let NMBLED=24;
+		let i;
+		let n;
+		let m;
+		let s;
+		let c;
+		let obj = ['black', 0, 0, 'red', 1, 1, 'black', 3, 12, 'green', 4, 24, 'black', 2, 24, 'blue', 3, 12, 'black', 3, 12]; // couleur sens depart
+		let ind = 0;
+		let plsled;
+		let maxled;
+		
+		$('.led').css('background-color', 'black');		// eteint tout
+			
+		// -------------------- test du bandeau r -> g -> b -------------------------
+		
+		c = obj[ind];
+		s = obj[ind+1];
+		n = obj[ind+2];
+		affiche();
+		
+		// -------------------------- deplacement voiture ---------------------------
+				
 		$('#av').click
 		(
 			function()
@@ -23,31 +45,60 @@ $
 				$('#voiture').animate({left:"800px"}, 6000, 'linear');
 			}
 		)
- 
+
+		// ------- colore le bandeau en fonction de la position de la voiture ---------
+		
 		function colorier()
 		{
-			//var n = div.queue( "fx" );
-			//$( "span" ).text( n.length );
 			if ($('#voiture').css('left') < '250px')
-				$('.led').css('background-color', 'red');
+				$('.led').css('background-color', 'darkred');
 			else if ($('#voiture').css('left') < '320px')
-				$('.led').css('background-color', 'green');
+				$('.led').css('background-color', 'darkgreen');
 			else  if ($('#voiture').css('left') < '620px')
-				$('.led').css('background-color', 'blue');
+				$('.led').css('background-color', 'darkblue');
 			else
 				$('.led').css('background-color', 'black');
 				
 			setTimeout(colorier, 100);
 		}
-		
-		colorier();
-		
- 		/*
-		console.log($('#voiture').css('left'));
-		if ($('#voiture').css('left') > '750px')
-			$('.led').css('background-color', 'black');
-		if ($('#voiture').css('left') < '500px')
-			$('.led').css('background-color', 'red');
-		*/
+ 
+		function affiche()
+		{
+			if ((n > 0) && (n < NMBLED+1))
+			{
+				$('#voyant .led:nth-child('+n+')').css('background-color', c);
+				if (s >= 3)
+				{
+					m = NMBLED-n-1;
+					$('#voyant .led:nth-child('+m+')').css('background-color', c);
+				}
+				
+				if ((s%2) == 1)
+					n++;
+				else
+					n--;
+			}
+			else
+			{
+				if (ind < 21)
+				{
+					ind+=3;
+					
+					c = obj[ind];
+					s = obj[ind+1];
+					n = obj[ind+2];
+				}
+				else
+				{
+					ind = 99;
+					$('.led').css('background-color', 'black');		// eteint tout
+				}
+			}
+			
+			if (ind != 99)
+				setTimeout(affiche, 100);
+			else
+				colorier();
+		}
 	}
 );
